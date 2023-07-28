2ed9423d432f0b2452e6f57a7243064771763482
Gabriel Galera <gabgalera@hotmail.com>
2023-07-28 09:09:27 -0300

    Seções 1 à 5

---
+++ src/pages/PageSettings.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page padding>
+    <p>Settings Page</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .border-grey {
+    border: 1px solid grey;
+  }
+  input, button {
+    font-size: 23px;
+  }
+  .error {
+    color: red;
+    background: pink;
+  }
+</style>
