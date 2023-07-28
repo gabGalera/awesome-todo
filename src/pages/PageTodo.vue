2cd23bab3521297b0e95f017c87b3aee6cb69ecd
Gabriel Galera <gabgalera@hotmail.com>
2023-07-28 14:47:07 -0300

    aulas 7.1, 7.2, 7.3 e 7.4

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -49,33 +49,11 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  data () {
-    return {
-      tasks: [
-        {
-          id: 1,
-          name: 'Go to shop',
-          completed: false,
-          dueDate: '2019/05/12',
-          dueTime: '18:30'
-        },
-        {
-          id: 2,
-          name: 'Get bananas',
-          completed: false,
-          dueDate: '2019/05/13',
-          dueTime: '14:00'
-        },
-        {
-          id: 3,
-          name: 'Get apples',
-          completed: false,
-          dueDate: '2019/05/14',
-          dueTime: '16:00'
-        }
-      ]
-    }
+  computed: {
+    ...mapGetters('tasks', ['tasks'])
   }
 }
 </script>
