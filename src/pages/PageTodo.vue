64904d76446082aafe7ecc97d1d8cc283f9a82de
Gabriel Galera <gabgalera@hotmail.com>
2023-07-28 09:22:18 -0300

    aula 6.1

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,12 +1,50 @@
 <template>
-  <q-page padding>
-    <p>Todo Page</p>
+  <q-page class="q-pa-md">
+    <q-list bordered>
+      <q-item-label header>General</q-item-label>
+
+      <q-item
+        v-for="task in tasks"
+        :key="task.id"
+        @click="task.completed = !task.completed"
+        clickable
+        v-ripple>
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ task.name }}</q-item-label>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: 'Go to shop',
+          completed: false
+        },
+        {
+          id: 2,
+          name: 'Get bananas',
+          completed: false
+        },
+        {
+          id: 3,
+          name: 'Get apples',
+          completed: false
+        }
+      ]
+    }
+  }
 }
 </script>
 
