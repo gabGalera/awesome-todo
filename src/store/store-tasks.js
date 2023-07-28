6c87639b92b3548e982d88d50ae79599044c2c72
Gabriel Galera <gabgalera@hotmail.com>
2023-07-28 15:31:02 -0300

    aula 8.1

---
+++ src/store/store-tasks.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-empty-pattern */
+const state = {
+  tasks: {
+    ID1: {
+      name: 'Go to shop',
+      completed: false,
+      dueDate: '2019/05/12',
+      dueTime: '18:30'
+    },
+    ID2: {
+      name: 'Get bananas',
+      completed: false,
+      dueDate: '2019/05/13',
+      dueTime: '14:00'
+    },
+    ID3: {
+      name: 'Get apples',
+      completed: false,
+      dueDate: '2019/05/14',
+      dueTime: '16:00'
+    }
+  }
+}
+
+const mutations = {
+  updateTask (state, payload) {
+    Object.assign(state.tasks[payload.id], payload.updates)
+  }
+}
+
+const actions = {
+  updateTask ({ commit }, payload) {
+    commit('updateTask', payload)
+  }
+}
+
+const getters = {
+  tasks: (state) => {
+    return state.tasks
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+}
