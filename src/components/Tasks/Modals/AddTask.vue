8c5409d25ab0a992de4dc7382c2e5a4a31fbda74
Gabriel Galera <gabgalera@hotmail.com>
2023-08-02 10:16:33 -0300

    aula 9.2

--- src/components/Tasks/Modals/AddTask.vue
+++ src/components/Tasks/Modals/AddTask.vue
@@ -1,7 +1,17 @@
 <template>
     <q-card>
-        <q-card-section>
-          <div class="text-h6">Alert</div>
+        <q-card-section
+          class="row"
+        >
+          <div class="text-h6">Add Task</div>
+          <q-space></q-space>
+          <q-btn
+            flat
+            round
+            dense
+            icon="close"
+            v-close-popup
+          />
         </q-card-section>
 
         <q-card-section class="q-pt-none">
@@ -9,7 +19,12 @@
         </q-card-section>
 
         <q-card-actions align="right">
-          <q-btn flat label="OK" color="primary" v-close-popup />
+          <q-btn
+            flat
+            label="Save"
+            color="primary"
+            v-close-popup
+          />
         </q-card-actions>
       </q-card>
 </template>
